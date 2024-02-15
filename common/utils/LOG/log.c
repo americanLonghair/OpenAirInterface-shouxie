c33451fcf13da9a0d1701a582536798d83395cfb
americanLonghair <155827612+americanLonghair@users.noreply.github.com>
2024-02-15 15:12:47 +0100

    Add files via upload

---
+++ common/utils/LOG/log.c
@@ -0,0 +1,20 @@
+//
+// Created by tonyy on 2024/2/14.
+//
+
+/*
+ * Liscensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ *
+ *
+ * ! \file log.h
+ *
+ */
+
+
+#define _GNU_SOURCE
+//#define LOG_TEST 1
+#ifndef __LOG_H__
+
+#define
